0a6eef336ae77d07391ddb6cad0c8a773d4f8c16
GyFish <geyu2227@163.com>
2019-05-23 01:13:45 +0800

    flow editor 第二天

--- web/src/components/flow/Maker.tsx
+++ web/src/components/flow/Maker.tsx
@@ -1,18 +1,52 @@
-import { CreateElement, VNode } from 'vue'
+import Vue, { CreateElement, VNode } from 'vue'
+import G6 from '@antv/g6'
 
 export default class Maker {
-  render(h?: CreateElement): VNode {
+
+  html: any
+  
+  render(h: CreateElement, commandHandler: any): VNode {
     return (
-      <div>
-        <el-dropdown trigger="click">
-          <el-button class="addButton" type="success" icon="el-icon-plus" />
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>任务节点</el-dropdown-item>
-            <el-dropdown-item>审核节点</el-dropdown-item>
-            <el-dropdown-item>分支节点</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-      </div>
+      <el-dropdown trigger="click" onCommand={commandHandler}>
+        <el-button class="addButton" type="success" icon="el-icon-plus" />
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command="task">任务节点</el-dropdown-item>
+          <el-dropdown-item command="check">审核节点</el-dropdown-item>
+          <el-dropdown-item command="branch">分支节点</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
     )
+  }
+
+  register(option: {}) {
+    const html = G6.Util.createDOM(`<div>`)
+    G6.registerNode('starter', {
+      draw(item: any) {
+        const group = item.getGraphicGroup()
+        return group.addShape('dom', {
+          attrs: {
+            x: 0,
+            y: 0,
+            width: 25,
+            height: 25,
+            html
+          }
+        })
+      }
+    })
+    this.html = html
+  }
+
+  createDom(callBack: Function) {
+    callBack.call(this)
+  }
+
+  mountVNode(h: CreateElement, commandHandler: any, option: {}) {
+    let vueNode = Vue.extend({
+      render: () => {
+        return this.render(h, commandHandler)
+      }
+    })
+    new vueNode().$mount(this.html)
   }
 }
--- web/src/views/FlowEditor.vue
+++ web/src/views/FlowEditor.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flow-editor">
     <el-container>
-
       <!-- 画板 -->
       <el-main>
         <div ref="page" class="flow-page"></div>
@@ -10,12 +9,7 @@
       <!-- 右侧配置面板 -->
       <el-aside>
         <div class="flow-config">
-          <flow-config
-            :nodeData="nodeData"
-            @addLine="addLine"
-            @updateVueModel="updateVueModel"
-            @saveDefinition="saveDefinition"
-          ></flow-config>
+          <flow-config :nodeData="nodeData" @saveDefinition="saveDefinition"></flow-config>
         </div>
       </el-aside>
     </el-container>
@@ -67,13 +61,20 @@ export default class FlowEditor extends Vue {
   lx: any = 0
   ly: any = 0
 
+  // starter 索引
+  starterIndex: any = 0
+
   //== vue 狗子 =====================================
 
   mounted() {
     this.createEditor()
     this.handleRead()
+    this.handleNodeClick()
 
     this.drawStartNode()
+
+    // 延迟加载 vnode，需要等到 G6 渲染完毕
+    setTimeout(this.drawStarterButton, 10)
   }
 
   //== 绘图方法 =====================================
@@ -87,11 +88,21 @@ export default class FlowEditor extends Vue {
     })
   }
 
+  // 渲染数据
+  handleRead(graphData: any = {}) {
+    this.graph.read(graphData)
+  }
+
+  handleNodeClick() {
+    this.graph.on('node:click', (ev: any) => {
+      let nodeId = ev.item.id
+    })
+  }
+
   // 绘制开始节点
   drawStartNode() {
-
     G6.registerNode('startNode', {
-      draw(item: any){
+      draw(item: any) {
         const group = item.getGraphicGroup()
         const model = item.getModel()
         let keyShape = group.addShape('circle', {
@@ -100,7 +111,8 @@ export default class FlowEditor extends Vue {
             y: 0,
             r: 30,
             fill: '#ECF5FF',
-            stroke: '#C6E2FF'
+            stroke: '#C6E2FF',
+            lineWidth: 2
           }
         })
         group.addShape('text', {
@@ -108,7 +120,7 @@ export default class FlowEditor extends Vue {
             x: 0,
             y: 0,
             fill: '#333',
-            text: '点击开始',
+            text: '开始',
             textBaseline: 'middle',
             textAlign: 'center'
           }
@@ -121,149 +133,43 @@ export default class FlowEditor extends Vue {
       id: 'startNode',
       shape: 'startNode',
       x: 400,
-      y: 50,
+      y: 50
     })
-
-    this.graph.on('node:click', (ev: any) => {
-      let nodeId = ev.item.id
-      if (nodeId == 'startNode') {
-        this.drawAddButton()
-        this.mountStart()
-        this.startLine()
-      }
-    })
-
   }
 
   // 绘制增加节点
-  drawAddButton() {
+  drawStarterButton() {
+    console.log('drawStarterButton')
 
-    G6.registerNode('addButton', {
-      draw(item: any){
-        const group = item.getGraphicGroup();
-        const html = G6.Util.createDOM(`<div id="add">`);
-        return group.addShape('dom', {
-          attrs: {
-            x: 0,
-            y: 0,
-            width: 28,
-            height: 28,
-            html
-          }
-        })
-      }
+    let maker = new Maker()
+    let id = 'starter' + this.starterIndex++
+
+    // 注册
+    maker.register()
+
+    // 创建 dom
+    maker.createDom(() => {
+      this.graph.add('node', {
+        id,
+        shape: 'starter',
+        x: 400 - 25 / 2,
+        y: 100
+      })
     })
 
-    let addButton = {
-      id: 'addButton',
-      shape: 'addButton',
-      x: 400 - 28 / 2,
-      y: 100,
-    }
-    this.graph.add('node', addButton)
-  
-  }
+    // 挂载 vnode
+    maker.mountVNode(this.$createElement, this.addFlowNode)
 
-  startLine() {
+    // 绘制连线
     this.graph.add('edge', {
-      id: 'edge1',
-      target: 'addButton',
-      source: 'startNode'
+      id: 'start-line',
+      source: 'startNode',
+      target: id
     })
   }
 
-  mountStart() {
-    let vueNode = Vue.extend({
-      render: () => { return new Maker().render(this.$createElement) }
-      // render: h => h('el-button', {
-      //   props: {
-      //     type: 'success',
-      //     icon: 'el-icon-plus',
-      //     circle: true,
-      //   }
-      // })
-    })
-    new vueNode().$mount('#add')
-  }
-
-  // 获取节点
-  getNode(ev: any) {
-    console.log('点击节点')
-  }
-
-  // 增加节点
-  addNode(event: any, nodeType: string) {
-    console.log('增加新节点')
-
-    let nodeName = ''
-    let nodeIcon = ''
-    if (nodeType == 'success') {
-      nodeName = '开始'
-      nodeIcon = 'el-icon-circle-plus'
-    }
-    if (nodeType == 'primary') {
-      nodeName = '任务节点'
-      nodeIcon = 'el-icon-tickets'
-    }
-    if (nodeType == 'warning') {
-      nodeName = '审核节点'
-      nodeIcon = 'el-icon-success'
-    }
-    if (nodeType == 'danger') {
-      nodeName = '结束'
-      nodeIcon = 'el-icon-error'
-    }
-
-    let id: string = 'node-' + new Date().getTime().toString()
-    let g6model = {
-      id,
-      shape: id,
-      x: event.clientX - 320,
-      y: event.clientY - 56
-    }
-    let nodeModel = {
-      id,
-      nodeName,
-      nodeType,
-      nodeIcon
-    }
-
-    // 向图内创建新节点
-    new G6Register().elButton(id)
-    let node = this.graph.add('node', g6model)
-
-    // 挂载节点组件
-    this.renderVueNode(nodeModel)
-
-    // 同步数据
-    this.nodeData.model = node
-    this.nodeData.dataMap = node.dataMap
-    this.nodeData.node = nodeModel
-    this.nodeData.nodeMap[id] = nodeModel
-
-    console.log('>> 更新 nodeData = ', this.nodeData)
-  }
-
-  // 修该节点属性
-  updateVueModel() {
-    console.log('updateVueModel')
-    // 更新 vue 节点
-    this.renderVueNode(this.nodeData.node)
-  }
-
-  // 增加连线
-  addLine(newLine: any) {
-    this.graph.add('edge', {
-      shape: 'line',
-      source: newLine.source,
-      target: newLine.target,
-      endArrow: true
-    })
-  }
-
-  // 渲染数据
-  handleRead(graphData: any = {}) {
-    this.graph.read(graphData)
+  addFlowNode(type: string) {
+    alert(type)
   }
 
   // 点击事件
@@ -300,29 +206,6 @@ export default class FlowEditor extends Vue {
         y: ev.y + this.dy
       })
     })
-  }
-
-  // 在节点上挂载 vue 组件
-  renderVueNode(flowModel: any) {
-    console.log('>> renderVueNode = ', flowModel)
-    let vueNode = Vue.extend({
-      render: h =>
-        h(
-          'el-button',
-          {
-            attrs: {
-              id: flowModel.id
-            },
-            props: {
-              autofocus: true,
-              type: flowModel.nodeType,
-              icon: flowModel.nodeIcon
-            }
-          },
-          [flowModel.nodeName]
-        )
-    })
-    new vueNode().$mount('#' + flowModel.id)
   }
 
   //== api =====================================
